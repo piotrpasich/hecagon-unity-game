496ec0211061ed9ef0f038bf816221b16e795dbe
Piotr Pasich <piotr.pasich@gmail.com>
2019-02-26 11:02:59 +0100

    init

---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 600f;
+
+    public GameObject pointsObject;
+
+    float movement = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.deltaTime * -moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Score.Reset();
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private static int maxScoreValue = 0;
+
+    private static int currentScoreValue = 0;
+
+    public Text currentScoreText;
+    public Text maxScoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentScoreText.text = currentScoreValue.ToString();
+        maxScoreText. text = "max:" + maxScoreValue.ToString();
+    }
+
+    public static void AddPoint()
+    {
+        currentScoreValue += 1;
+        if (currentScoreValue > maxScoreValue) {
+            maxScoreValue = currentScoreValue;
+        }
+    }
+
+    public static void Reset()
+    {
+        currentScoreValue = 0;
+    }
+}
